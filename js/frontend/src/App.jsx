144fc8d89481a1c45d253678811a45691e307fe6
Victor G <victorgj95@outlook.com>
2024-01-17 17:32:18 +0100

    banner done

--- js/frontend/src/App.jsx
+++ js/frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 
+import "./style/index.scss";
+
 function App() {
   return (
-    <>
+    <div>
       <Outlet />
-      <div>Hihi</div>
-    </>
+    </div>
   );
 }
 
