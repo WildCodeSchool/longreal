a981b269d8a36525f516fdc25806f8721d889b49
lulb11 <bordeslucas11@gmail.com>
2024-01-18 15:08:13 +0100

    weatherproducts are working

--- js/frontend/src/components/ProductsCarousel.jsx
+++ js/frontend/src/components/ProductsCarousel.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react";
+import { React } from "react";
 import {
   MDBCarousel,
   MDBCarouselItem,
@@ -8,119 +8,20 @@ import { useTheContext } from "../context/context";
 
 function ProductsCarousel() {
   const { weatherProducts } = useTheContext();
-  console.log(weatherProducts);
-  const [products, setProducts] = useState();
-
-  useEffect(() => {
-    fetch("http://localhost:3310/api/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data))
-      .catch((err) => err);
-  }, []);
 
   return (
-    <div className="carousel-container">
-      {products ? (
-        <MDBCarousel showControls showIndicators>
-          {products.map((product) => {
-            return (
-              <MDBCarouselItem
-                itemId={products?.productId}
-                key={products?.productId}
-              >
-                <img src={product?.image} className="d-block w-100" alt="..." />
-                <MDBCarouselCaption>
-                  <h5 className="slides-label">First slide label</h5>
-                  <p className="slides-description">
-                    Nulla vitae elit libero, a pharetra augue mollis interdum.
-                  </p>
-                </MDBCarouselCaption>
-              </MDBCarouselItem>
-            );
-          })}
-          <MDBCarouselItem itemId={2}>
-            <img
-              src="https://mdbootstrap.com/img/new/slides/042.jpg"
-              className="d-block w-100"
-              alt="..."
-            />
-
+    <div>
+      <MDBCarousel showControls showIndicators>
+        {weatherProducts.map((product) => (
+          <MDBCarouselItem itemId={1} key={product?.productId}>
+            <img src={product?.image} className="d-block w-100" alt="..." />
             <MDBCarouselCaption>
-              <h5 className="slides-label">Second slide label</h5>
-              <p className="slides-description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </p>
+              <h5>First slide label</h5>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </MDBCarouselCaption>
           </MDBCarouselItem>
-          <MDBCarouselItem itemId={3}>
-            <img
-              src="https://mdbootstrap.com/img/new/slides/043.jpg"
-              className="d-block w-100"
-              alt="..."
-            />
-            <MDBCarouselCaption>
-              <h5 className="slides-label">Third slide label</h5>
-              <p className="slides-description">
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </MDBCarouselCaption>
-          </MDBCarouselItem>
-          <MDBCarouselItem itemId={4}>
-            <img
-              src="https://mdbootstrap.com/img/new/slides/043.jpg"
-              className="d-block w-100"
-              alt="..."
-            />
-            <MDBCarouselCaption>
-              <h5 className="slides-label">Third slide label</h5>
-              <p className="slides-description">
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </MDBCarouselCaption>
-          </MDBCarouselItem>
-          <MDBCarouselItem itemId={5}>
-            <img
-              src="https://mdbootstrap.com/img/new/slides/043.jpg"
-              className="d-block w-100"
-              alt="..."
-            />
-            <MDBCarouselCaption>
-              <h5 className="slides-label">Third slide label</h5>
-              <p className="slides-description">
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </MDBCarouselCaption>
-          </MDBCarouselItem>
-          <MDBCarouselItem itemId={6}>
-            <img
-              src="https://mdbootstrap.com/img/new/slides/043.jpg"
-              className="d-block w-100"
-              alt="..."
-            />
-            <MDBCarouselCaption>
-              <h5 className="slides-label">Third slide label</h5>
-              <p className="slides-description">
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </MDBCarouselCaption>
-          </MDBCarouselItem>
-          <MDBCarouselItem itemId={7}>
-            <img
-              src="https://mdbootstrap.com/img/new/slides/043.jpg"
-              className="d-block w-100"
-              alt="..."
-            />
-            <MDBCarouselCaption>
-              <h5 className="slides-label">Third slide label</h5>
-              <p className="slides-description">
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </MDBCarouselCaption>
-          </MDBCarouselItem>
-        </MDBCarousel>
-      ) : (
-        "Loading!"
-      )}
+        ))}
+      </MDBCarousel>
     </div>
   );
 }
